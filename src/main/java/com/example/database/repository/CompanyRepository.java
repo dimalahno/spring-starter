546e7964fd819f65e3787dab6dd4918a7dc48d0c
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-03-13 08:05:43 +0500

    Add owen annotation, and bpp

--- src/main/java/com/example/database/repository/CompanyRepository.java
+++ src/main/java/com/example/database/repository/CompanyRepository.java
@@ -1,15 +1,18 @@
 package com.example.database.repository;
 
+import com.example.bpp.InjectBean;
 import com.example.database.pool.ConnectionPool;
 
 public class CompanyRepository {
-    private final ConnectionPool connectionPool;
 
-    private CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+//    private CompanyRepository(ConnectionPool connectionPool) {
+//        this.connectionPool = connectionPool;
+//    }
+//
+//    public static CompanyRepository of(ConnectionPool connectionPool) {
+//        return new CompanyRepository(connectionPool);
+//    }
 }
