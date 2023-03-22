6b0ab83fbec4b197e556f688d8a81a2e6637c68d
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-03-22 11:22:42 +0500

    Configure beans with configuration

--- src/main/java/com/example/config/ApplicationConfiguration.java
+++ src/main/java/com/example/config/ApplicationConfiguration.java
@@ -1,7 +1,10 @@
 package com.example.config;
 
+import com.example.database.pool.ConnectionPool;
 import com.example.database.repository.CrudRepository;
+import com.example.database.repository.UserRepository;
 import com.example.web.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
@@ -19,5 +22,26 @@ includeFilters = {
 })
 public class ApplicationConfiguration {
 
+    @Bean("pool2")
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
 
+    @Bean
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 25);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
+
+    @Bean
+    public UserRepository userRepository3() {
+        ConnectionPool connectionPool1 = pool3();
+        ConnectionPool connectionPool2 = pool3();
+        ConnectionPool connectionPool3 = pool3();
+        return new UserRepository(pool3());
+    }
 }
