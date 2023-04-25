0f323320f73f493896b0d2c98eecd9b91f27b136
Dmitry.Lakhno <dimalahno@rambler.ru>
2023-04-25 19:55:00 +0500

    Configured properties

--- src/main/java/com/example/ApplicationRunner.java
+++ src/main/java/com/example/ApplicationRunner.java
@@ -1,15 +1,19 @@
 package com.example;
 
+import com.example.config.DataBaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApplicationRunner {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(ApplicationRunner.class, args);
         System.out.println(context.getBeanDefinitionCount());
+        System.out.println(context.getBean(DataBaseProperties.class));
     }
 }
 
